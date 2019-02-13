4f0f95e28f68ec1e432368ad313f3c83cc9a9cd6
Constant Dorian <21703363t>
2019-02-13 09:52:49 +0100

    correction js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
     document.getElementById('pseudo').addEventListener('blur', function(){
         let pseudo = document.insc.pseudo.value;
         if (validPseudo(pseudo) == false){
-            document.getElementById('mess_pseudo').textContent = 'Erreur de pseudo,
-            seulement lettres et chiffres';
+            document.getElementById('mess_pseudo').textContent = 'Erreur de pseudo, seulement lettres et chiffres';
         }
     });
 

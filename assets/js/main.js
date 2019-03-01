0bc56e48f3b7b4a848326915f7f17672ce6eb5d7
Constant Dorian <21703363t>
2019-03-01 16:09:57 +0100

    suppr weekend (datepicker)

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,20 @@ document.addEventListener('DOMContentLoaded', function(){
 
 jQuery(document).ready(function($){
 
+    function validDateInsc(date){
+        var dayInWeek = date.getDay();
+        if(dayInWeek == 0 || dayInWeek == 6)
+        return [false];
+        return [true];
+    }
+
+    var minDate = new Date(2019, 03, 01);
+    var maxDate = new Date(2019, 05, 01);
+
     $.datepicker.setDefaults($.datepicker.regional['fr']);
-    $('#datepicker').datepicker();
+    $('#datepicker').datepicker({
+        beforeShowDay: validDateInsc,
+        minDate : minDate,
+        maxDate : maxDate
+    });
 });
